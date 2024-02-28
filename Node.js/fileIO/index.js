8a231cd6f99b9ff2836c7677346c439f43851c5a
srikrishna80 <srikrishna@ibentos.com>
2024-02-28 11:43:13 +0530

    added the html write code

--- Node.js/fileIO/index.js
+++ Node.js/fileIO/index.js
@@ -2,15 +2,36 @@
 //  //_dirname is not defined in the es6 moduling.
 //reading files inside node.js
 
-import {readFile} from 'fs/promises';
+import { write } from 'fs';
+import {readFile,writeFile} from 'fs/promises';
+import { url } from 'inspector';
 
 console.log(import.meta.url); // current path 
 
 const filePath = new URL('./index.html',import.meta.url);
+// console.log(filePath);
 
-const contents = await readFile(filePath,{encoding:'utf-8'});
+let contents = await readFile(filePath,{encoding:'utf-8'});
 
-console.log(contents);
+const data = {
+    name:'Sanketh singh',
+    company:"Google",
+    age:25,
+}
+
+for(const [key,value] of Object.entries(data)){
+    contents = contents.replace(`{${key}}`,value);
+}
+
+
+//write file will modify the data in the path
+
+// await writeFile(filePath,contents);
+
+//creating new file and writing data into the file
+
+await writeFile(new URL('./newindex.html',import.meta.url),contents);
+// console.log(contents);
 
 //. -----> current folder
 //.. -----> one folder back
