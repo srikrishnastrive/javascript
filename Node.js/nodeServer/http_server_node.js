45548578b9d1e3366b25fe8b9131eafcf943bdab
srikrishna80 <srikrishna@ibentos.com>
2024-03-01 09:50:14 +0530

    added the http code

--- Node.js/nodeServer/http_server_node.js
+++ Node.js/nodeServer/http_server_node.js
@@ -3,6 +3,14 @@ const http = require('http');
 
 const server = http.createServer((req,res) => {
     console.log("New connection was created");
+    //res.end means ending the response
+    if(req.url === '/home'){
+        res.end("welcome home");
+    }
+    else {
+       res.end("Something something !!");
+    }
+  
 })
 
 let port = 3000;
